e31a8806e5040d431052d57975c2d5a49fe44f1c
Abhishek <abhishekkolapkar10@gmail.com>
2023-03-04 17:19:59 +0530

    movie app completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+import Home from './components/Home/Home'
+import MovieDetail from './components/MovieDetail/MovieDetail'
+import PageNotFound from './components/PageNotFound/PageNotFound'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='app'>
+      <Header />
+      <div className='container'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/movie/:imdbID' element={<MovieDetail />} />
+          <Route element={<PageNotFound />} />
+        </Routes>
+      </div>
+      <Footer />
+    </div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
+import { store } from "./features/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
